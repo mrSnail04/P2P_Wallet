c180e7465f05949b87209ce02de4071f2433294d
Maksim Bulauski <maks.bulavsky@gmail.com>
2022-07-23 01:00:10 +0300

    gitPages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import './App.module.scss'
 import { About } from './pages/About/About'
 import { Layout } from './components/Layout/Layout'
@@ -9,7 +9,6 @@ import { Help } from './pages/Help/Help'
 import { BuyCrypto } from './pages/BuyCrypto/BuyCrypto'
 
 function App () {
-  const navigate = useNavigate()
   return (
     <Routes>
       <Route element={<Layout/>}>
@@ -20,14 +19,14 @@ function App () {
         <Route path="crypto" element={<BuyCrypto/>}>
            <Route path=":cryptoOption/:currencyBuy/:currencySell" element= {<BuyCrypto/>} />
         </Route>
-        <Route path="*" element={navigate('/')} />
+        <Route path="*" element={<NotFound/>} />
       </Route>
     </Routes>
   )
 }
 export default App
-// const NotFound = () => {
-//   return (
-//     <div> 404 </div>
-//   )
-// }
+const NotFound = () => {
+  return (
+    <div> 404 </div>
+  )
+}
